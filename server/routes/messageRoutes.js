11bd2290c8c923b9cbf3e8d3e5dd3cc5e33ebb9b
tanyagupta-12 <tg10001000@gmail.com>
2026-02-12 22:23:47 +0530

    first commit

---
+++ server/routes/messageRoutes.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { protectRoute } from "../middleware/auth.js";
+import { getMessages, getUsersForSide, markMessageAsSeen, sendMessage } from "../controllers/messageController.js";
+
+const messageRouter = express.Router();
+messageRouter.get("/users",protectRoute,getUsersForSide);
+messageRouter.get("/:id",protectRoute,getMessages)
+messageRouter.put("/mark/:id",protectRoute,markMessageAsSeen);
+messageRouter.post("/send/:id",protectRoute,sendMessage);
+
+export default messageRouter;
---
+++ server/server.js
@@ -0,0 +1,41 @@
+import cors from "cors";
+import express from "express"
+import "dotenv/config"
+import http from "http"
+import { connectDB } from "./lib/db.js";
+import userRouter from "./routes/userRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
+import {Server} from "socket.io";
+import e from "cors";
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.json({limit : "4mb"}));
+app.use(cors());
+
+export const io = new Server(server,{
+    cors : {origin : "*" }
+})
+export const userSocketMap = {};
+io.on("connection",(socket) =>{
+ const userId = socket.handshake.query.userId;
+  console.log("User Connected",userId);
+
+  if(userId) userSocketMap[userId] = socket.id;
+
+  io.emit("getOnlineUsers",Object.keys(userSocketMap));
+  socket.on("disconnect",() =>{
+    console.log("User Disconnected",userId);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers",Object.keys(userSocketMap))
+  })
+})
+app.use("/api/status",(req,res) => res.send("Server is live"));
+app.use("/api/auth",userRouter)
+app.use("/api/messages",messageRouter)
+
+await connectDB();
+
+const PORT = process.env.PORT  || 5000;
+server.listen(PORT,() => console.log ("Server is running on PORT :" + PORT))
